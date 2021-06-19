1eba566b01a18995e732594ef0ecaa775a205bf0
Johny <johny.hoffman@gmail.com>
2021-06-19 06:50:23 -0500

    Add node serverless api

---
+++ serverless_apis/aws-node-rest-api-with-dynamodb/todos/create.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.task !== "string") {
+    console.error("Validation Failed");
+    callback(null, {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: "Couldn't create the todo item.",
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: data.id,
+      task: data.task,
+      completed: false,
+      created: timestamp,
+      updated: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {
+          "Content-Type": "text/plain",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: "Couldn't create the todo item.",
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+    callback(null, response);
+  });
+};
---
+++ serverless_apis/aws-node-rest-api-with-dynamodb/todos/get.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {
+          "Content-Type": "text/plain",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: "Couldn't fetch the todo item.",
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+    callback(null, response);
+  });
+};
